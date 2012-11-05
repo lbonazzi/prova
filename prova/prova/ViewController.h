8d20ef5e869032ce7ddd418733ae35ccf472054f
Lorenzo Bonazzi <lorenzo@bonazzisoftware.it>
2012-11-05 19:02:09 +0100

    inizio

---
+++ prova/prova/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  prova
+//
+//  Created by Lorenzo Bonazzi on 11/5/12.
+//  Copyright (c) 2012 Lorenzo Bonazzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
